4a501e70e9630c0ca58d9c1f42e3df50f31cdcba
username <rusen.seran@gmail.com>
2022-06-06 00:40:13 -0700

    html and css updates

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,9 @@
 # BlackJack
+##your planning github repo should include these things on its README.md (you can make this file on your computer):
+The name of your game
+rules of your game (how do you win? how do you lose?)
+wireframes (photos of how the game would look)
+pseudocode of how the game would work in JavaScript
+helpful links (websites about your game, useful resources you would like to use or learn about, etc.)
+you can follow this guide to make a well-formatted Readme using Markdown, the language of .md!
+https://www.markdownguide.org/basic-syntax/##
---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*----- constants -----*/
+const CARD_DECK = [];
+const MAX_SUM = 21
+const DEALER_MAX = 17
+
+/*----- app's state (variables) -----*/
+
+
+const gameButtons = document.querySelectorAll("button");
+
+
+/*----- cached element references -----*/
+let dealerSum = document.getElementById("d-sum");
+let playerSum = document.getElementById("p-sum");
+let gameStatus = document.getElementById("game-status")
+
+/*----- event listeners -----*/
+// newgame button
+// hit button
+// stand button
+
+// gameButtons.addEventlistener("click", handleClick)
+
+// // /*----- functions -----*/
+// getNewCard
+// newGame
+// initGame
+// handleClick
+// checkWin
+// checkTie
+function handleClick(evt) {
+    if (evt.target.innerText === "HIT") {
+
+    } else if (evt.target.innerText === "STAY") {
+
+    } else if (evt.target.innerText === "NEW GAME") {
+        newGame()
+    }
+
+
+
+
+}
+
+
+console.log(gameStatus)
+console.log(dealerSum)
+console.log(playerSum)
+console.log(gameButtons)
